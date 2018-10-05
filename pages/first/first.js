52a71a12bb065d66a610cbb58c72b53795ce4719
kafka <a50541853@gmail.com>
2018-10-05 18:45:52 +0800

    自媒体实战

--- pages/first/first.js
+++ pages/first/first.js
@@ -1,21 +1,30 @@
-// pages/layout/layout.js
+// pages/first/first.js
 Page({
 
   /**
    * 页面的初始数据
    */
-    data: {
+  data: {
+    swiper:{
+      indicatorDots:true,
+      autoplay: true, 
+      interval: 3000,
+      duration: 1000,
       imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ]},
+        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ],
+    },
+    content_items:['','','',''],
+    test_item: ['', '', '', '']
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+        
   },
 
   /**
